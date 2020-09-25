48970edc45f278bb222b17046a0852c6f4bd47b0
Aditya Verma <aaadarths2357@gmail.com>
2020-09-25 19:48:17 +0530

    Welcome to Email Registration System

---
+++ src/main/java/com/bridgelabz/pattern/emailregistration/App.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.pattern.emailregistration;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Email Registration System" );
+    }
+}
